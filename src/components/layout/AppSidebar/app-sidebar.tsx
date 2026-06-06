138d54a3ac5c55ed539800e9b3fbb86367df5266
Wilcar Ortiz <ortizcolpaswilcardaniel@gmail.com>
2026-06-05 20:15:30 -0500

    Actualizacion del logotipo GEOASISTENCIA

--- src/components/layout/AppSidebar/app-sidebar.tsx
+++ src/components/layout/AppSidebar/app-sidebar.tsx
@@ -24,7 +24,7 @@ export function AppSidebar({
         <div className="flex items-center justify-center pt-3">
           <div className="relative h-[100px] w-[100px]">
             <Image
-              src="/assets/logotipoGeoAsistencia.png"
+              src="/assets/logo.png"
               alt="GeoAsistencia logo"
               fill
               sizes="100px"
